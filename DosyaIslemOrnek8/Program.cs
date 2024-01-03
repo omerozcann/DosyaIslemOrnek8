f02fb9f11de989536006b8203cdd72b89ac19688
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-03 11:07:10 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dosyaların silinmesi
+            File.Delete("abc.txt"); //belirtilen dosyayı siler
+
+           //Dosyaların kopyalanması
+            File.Copy("d:\\abc.txt", "e:\\abc.txt");//d sürücüsündeki abc.txt yi,
+           //e sürücüsüne kopyalar
+
+           //Klasörlerin oluşturulması
+            Directory.CreateDirectory("d:\\bozkır myo");
+
+            //Klasörlerin silinmesi
+            Directory.Delete("d:\\bozkır myo");
+        }
+    }
+}
